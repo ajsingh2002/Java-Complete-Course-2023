3295b39f730ba76f9309730e9fac3f80ae9f1eff
Ajay <ajaysingh16112002@gmail.com>
2023-03-28 23:11:44 +0530

    Adding Files

--- src/NumberOfDays.java
+++ src/NumberOfDays.java
@@ -6,6 +6,14 @@ public class NumberOfDays {
         int month = scan.nextInt();
         System.out.println("Enter Year");
         int year = scan.nextInt();
-        )
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            System.out.print("Total days in the month is 31");
+        else if((month == 2) && ((year%400==0) || (year%4==0 && year%100!=0)))
+            System.out.print("Total days in the month is 29");
+        else if(month == 2)
+            System.out.print("Total days in the month is 28");
+        else
+            System.out.println("Total days in the month is 30");
+
     }
 }
