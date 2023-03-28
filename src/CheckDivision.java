2dd8612211aebd3a3a461d566f52441f09a6b4b5
Ajay <ajaysingh16112002@gmail.com>
2023-03-29 00:49:36 +0530

    Adding Files

--- src/CheckDivision.java
+++ src/CheckDivision.java
@@ -1,5 +1,28 @@
+import java.util.*;
 public class CheckDivision  {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter marks");
+        int marks = scan.nextInt();
+        int per = marks/5;
+        if (per>=60)
+            System.out.println("Division 1 ");
+        else if(per>=45 && per<60)
+            System.out.println("Division 2 ");
+        else if(per>=30 && per <45)
+            System.out.println("Division 3");
+        else
+            System.out.println("Fail");
     }
 }
+/*
+total no = 500
+number = 461
+
+percentage = (461/500)*100;
+if(percentage >= 60){
+sout("division one");
+sout("percenteage is "+pecentage+"%");
+}
+sout()
+ */
